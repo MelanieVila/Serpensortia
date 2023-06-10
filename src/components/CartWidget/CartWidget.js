1b688ad47bfdff08ffb1355307d8641968372926
Melanie Vila <melanievila19@hotmail.com>
2023-06-10 16:31:04 -0300

    navbar

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import "./CartWidget.css"
+
+const CartWidget = () => {
+    return (
+        <div className='carrito'>
+            <FontAwesomeIcon icon={faCartShopping} />
+            <p>0</p>
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import CartWidget from "../CartWidget/CartWidget"
+import logo from "./assets/logo.png"
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="nav__container sombra">
+                <img src={logo} className="logo__imagen" alt="Logo"/>
+                <div>
+                    <a href="/">Suéteres</a> ♦ <a href="/">Bufandas</a> ♦ <a href="/">Túnicas</a> ♦ <a href="/">Corbatas</a> ♦ <a href="/">Varitas</a>
+                </div>                
+                <div><CartWidget /></div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
